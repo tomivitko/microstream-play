ec111e9f611321756cdbb2e4fe0cae461558e0d6
Tomislav Vitko <tvitko@conet.hr>
2023-12-12 09:36:20 +0100

    cannot change values in records

--- src/main/java/com/example/microstreamsplay/Book.java
+++ src/main/java/com/example/microstreamsplay/Book.java
@@ -1,4 +1,17 @@
 package com.example.microstreamsplay;
 
-public record Book(String name, Integer year, Author author, String test) {
+
+import one.microstream.persistence.binary.types.Binary;
+import one.microstream.persistence.binary.types.BinaryTypeHandler;
+
+public record Book(String name, String year, Author author, String test) {
+
+    static BinaryTypeHandler<Book> provideTypeHandler(){
+        return Binary.TypeHandler(
+                Book.class,
+                Binary.Field_int("year",
+                        e -> Integer.parseInt(e.year),
+                        (e, value) -> e.year = String.valueOf(value))
+        );
+    }
 }
